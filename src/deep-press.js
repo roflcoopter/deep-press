753123ebb003f27a84aa288b41ccee2f84c84f78
Jesper Nilsson <jespernilsson93@hotmail.se>
2019-09-02 14:18:35 +0000

    fix event handlers

--- src/deep-press.js
+++ src/deep-press.js
@@ -3,10 +3,10 @@ import {
 } from 'custom-card-helpers';
 
 import Pressure from 'pressure';
-import * as Bowser from 'bowser';
 
 customElements.whenDefined('card-tools').then(() => {
   let cardTools = customElements.get('card-tools');
+  const HaCard = customElements.get('ha-card');
 
   // Set global config
   const defaults = {
@@ -17,16 +17,6 @@ customElements.whenDefined('card-tools').then(() => {
   if('ontouchforcechange' in document === false && config.enable_unsupported == false){
     return; // disable if device doesnt support force-touch
   }
-
-  // https://community.home-assistant.io/t/lovelace-button-card/65981/1933
-  const br = Bowser.getParser(window.navigator.userAgent);
-  const isCrazyBrowser = br.satisfies({
-    mobile: {
-      safari: '>=13',
-    },
-  });
-  const ios13 = new RegExp('^13\\..*', 'gm');
-  const isCrazyBrowser2 = br.getOSName() === 'iOS' && (br.getOSVersion().match(ios13) ? true : false);
 
   const setModalBehaviour = function(enable_clicks) {
     var modal = document.querySelector("body > home-assistant").shadowRoot.querySelector("ha-more-info-dialog");
@@ -71,20 +61,19 @@ customElements.whenDefined('card-tools').then(() => {
     );
   });
 
-  const HaCard = customElements.get('ha-card');
+  const simulateClick = function(targetNode) {
+      function triggerEvent(targetNode, eventType) {
+          var clickEvent = document.createEvent('MouseEvents');
+          clickEvent.initEvent(eventType, true, true);
+          targetNode.dispatchEvent(clickEvent);
+      }
+      ['click','touchend'].forEach(function(eventType) {
+          triggerEvent(targetNode, eventType);
+      });
+  };
 
-  const findConfig = function(node) {
-    if(node.config)
-      return node.config;
-    if(node._config)
-      return node._config;
-    if(node.host)
-      return findConfig(node.host);
-    if(node.parentElement)
-      return findConfig(node.parentElement);
-    if(node.parentNode)
-      return findConfig(node.parentNode);
-    return null;
+  const stopProp = function(event) {
+    event.stopPropagation();
   };
 
   const addCover = function(root, config) {
@@ -95,19 +84,19 @@ customElements.whenDefined('card-tools').then(() => {
     // Create a cover which captures the deep press
     root.cover = document.createElement("div");
     root.cover.setAttribute("id", "deep-press-cover");
-    root.cover.onmouseup = function(event) { if (this.hold) { event.stopPropagation(); } };
-    root.cover.ontouchend = function(event) { if (this.hold) { event.stopPropagation(); } };
-    root.cover.ontouchstart = function(event) { event.stopPropagation(); };
-    if (!isCrazyBrowser && !isCrazyBrowser2) {
-      root.cover.onmousedown = function(event) { event.stopPropagation(); };
-      root.cover.onclick = function(event) { if (this.hold) { event.stopPropagation(); } };
-    }
-
     root.cover.setAttribute(
       "style",
       "position:absolute; top:0; left:0; width:100%; height: 100%;"
     );
     root.appendChild(root.cover);
+
+    // Stop propagation of all events. A click will be simulated when we deem necessary
+    root.cover.addEventListener('touchstart', stopProp, { passive: true });
+    root.cover.addEventListener('touchend', stopProp, { passive: true });
+    root.cover.addEventListener('touchcancel', stopProp, { passive: true });
+    root.cover.addEventListener('mousedown', stopProp, { passive: true });
+    root.cover.addEventListener('mouseup', stopProp, { passive: true });
+    root.cover.addEventListener('click', stopProp, { passive: true });
 
     Pressure.set(root.cover, {
       start: function(event){
@@ -136,11 +125,32 @@ customElements.whenDefined('card-tools').then(() => {
         }
       },
 
+      // Called when deep press ends
       end: function(){
         this.view.style.webkitFilter= 'blur(0px)';
+        // If hold wasnt detected, simulate a click on the element to trigger default actions of the underlying card
+        if (!this.hold) {
+          simulateClick(root);
+        };
+        this.hold = false;
+        this.deep_press = false;
       },
     });
   }
+
+  const findConfig = function(node) {
+    if(node.config)
+      return node.config;
+    if(node._config)
+      return node._config;
+    if(node.host)
+      return findConfig(node.host);
+    if(node.parentElement)
+      return findConfig(node.parentElement);
+    if(node.parentNode)
+      return findConfig(node.parentNode);
+    return null;
+  };
 
   var cached_update = HaCard.prototype.update;
   HaCard.prototype.update = function(e){
